590676732be6aa0c20795340d265a4a1d53450da
vincent <3millestone@gmail.com>
2024-06-13 09:31:07 +0200

    correctrion code

--- Big_data/1_Nettoyage_donnees.R
+++ Big_data/1_Nettoyage_donnees.R
@@ -17,21 +17,7 @@ compter_na_et_vide <- function(data) {
 }
 
 
-#-----------------------------------------------------------------------------------------------------
-#              HARMONISATION
-#-----------------------------------------------------------------------------------------------------
-#ENLEVER majuscule
-data$fk_stadedev <- data$fk_stadedev %>% str_to_lower()
-data$fk_port <- data$fk_port %>% str_to_lower()
-data$fk_pied <- data$fk_pied  %>% str_to_lower()
-data$fk_arb_etat <- data$fk_arb_etat %>% str_to_lower()
-data$fk_pied <- data$fk_pied %>% str_to_lower()
-unique(data$fk_arb_etat)
-
-#RAJOUTER accent à e
-data$fk_port[ data$fk_port == "couronné"] <- "couronne"
-
-print("AVANT CLEAN")
+print("AVANT NETTOYAGE")
 compter_na_et_vide(data)
 ncol(data)
 nrow(data)
@@ -39,7 +25,7 @@ nrow(data)
 #-----------------------------------------------------------------------------------------------------
 #              SUPPRIMER LES COLLONES INUTILES
 #-----------------------------------------------------------------------------------------------------
-data <- select(data,-c(src_geo, created_date, created_user, id_arbre, commentaire_environnement,clc_nbr_diag, last_edited_user, last_edited_date, CreationDate, Creator, EditDate, Editor, nomlatin, fk_nomtech, fk_prec_estim))
+data <- select(data,-c(src_geo, created_date, created_user, id_arbre, commentaire_environnement,clc_nbr_diag, last_edited_user, last_edited_date, CreationDate, Creator, EditDate, Editor, GlobalID, nomlatin, fk_nomtech, fk_prec_estim))
 
 #-----------------------------------------------------------------------------------------------------
 #              REMPLACER LES VIDES
@@ -104,21 +90,36 @@ data <- data %>%   filter(!(is.na(fk_situation)))
 # SUPPRIMER les arbres sans feuillage
 data <- data %>%   filter(!(is.na(feuillage)))
 
-# SUPPRIMER les arbres sans feuillage
-data <- data %>%   filter(!(is.na(remarquable)))
 #-----------------------------------------------------------------------------------------------------
 #              CORRIGER
 #-----------------------------------------------------------------------------------------------------
+#ENLEVER les majuscules  pour :
+data$fk_stadedev <- data$fk_stadedev %>% str_to_lower()#le stade  de développment
+data$fk_port <- data$fk_port %>% str_to_lower()#le port
+data$fk_pied <- data$fk_pied  %>% str_to_lower()# le pied
+data$fk_arb_etat <- data$fk_arb_etat %>% str_to_lower()# l'état
+unique(data$fk_arb_etat)
+
+#RAJOUTER l'accent à e
+data$fk_port[ data$fk_port == "couronné"] <- "couronne"
+
 # CORRIGER les arbres EN PLACE avec une date d'abattage par ABATTU
 data <- data %>%  mutate(fk_arb_etat = if_else(!is.na(dte_abattage) & fk_arb_etat == "en place", "abattu", fk_arb_etat))
+
+# CORRIGER les  secteurs mal orthographiés
+data <- data %>%  mutate(clc_secteur = if_else(clc_secteur == "square des marronniers" | clc_secteur == "square des marronniers ", "Square des Marronniers", clc_secteur))
+data <- data %>%  mutate(clc_secteur = if_else(clc_secteur == "Rue de la Fere", "Rue de la Fère", clc_secteur))
+data <- data %>%  mutate(clc_secteur = if_else(clc_secteur == "rue Hertz", "Rue Hertz", clc_secteur))
+data <- data %>%  mutate(clc_secteur = if_else(clc_secteur == "Griourt", "Gricourt", clc_secteur))
+data <- data %>%  mutate(clc_secteur = if_else(clc_secteur == "Avenue Pierre Choquart", "Avenue Pierre Chocquart", clc_secteur))
 #-----------------------------------------------------------------------------------------------------
 #              REMPLACER LES VALEURS INCONUS
 #-----------------------------------------------------------------------------------------------------
 #REMPLACER villeca
-data <- data %>%  mutate(villeca = if_else(is.na(villeca), "cdq", villeca))
+data <- data %>%  mutate(villeca = if_else(is.na(villeca), "cdc", villeca))
 
 #REMPLACER clc_quartier
-data <- data %>%  mutate(clc_quartier = if_else(is.na(clc_quartier), "périphérie", clc_quartier))
+data <- data %>%  mutate(clc_quartier = if_else(is.na(clc_quartier), "Périphérie", clc_quartier))
 
 #REMPLACER fk_revetement
 data <- data %>%  mutate(fk_revetement = if_else(is.na(fk_revetement), "inconnu", fk_revetement))
@@ -135,31 +136,21 @@ data <- data %>%  mutate(fk_revetement = if_else(is.na(fk_revetement), "inconnu"
 #REMPLACER fk_situation
 #data <- data %>%  mutate(fk_situation = if_else(is.na(fk_situation), "inconnu", fk_situation))
 
-#REMPLACER dte_plantation
-data <- data %>%  mutate(dte_plantation = if_else(is.na(dte_plantation), "0", dte_plantation))
-
-#REMPLACER dte_abattage
-data <- data %>%  mutate(dte_abattage = if_else(is.na(dte_abattage), "0", dte_abattage))
-
 #REMPLACER feuillage
 #data <- data %>%  mutate(feuillage = if_else(is.na(feuillage), "inconnu", feuillage))
 
 #REMPLACER remarquable
-#data <- data %>%  mutate(remarquable = if_else(is.na(remarquable), "inconnu", remarquable))
+data <- data %>%  mutate(remarquable = if_else(is.na(remarquable), "non", remarquable))
 
 #-----------------------------------------------------------------------------------------------------
 #              SUPPRIMER LES COLLONES EN TROP
 #-----------------------------------------------------------------------------------------------------
-#data <- select(data,-c(dte_plantation, dte_abattage))
+data <- select(data,-c(dte_plantation, dte_abattage))
 
-
-
-print("APRES CLEAN")
+print("APRES NETTOYAGE")
 compter_na_et_vide(data)
 ncol(data)
 nrow(data)
-
-
 
 #-----------------------------------------------------------------------------------------------------
 #              CREER UN DATA QUANTITATIF POUR IA
@@ -175,15 +166,8 @@ data_ia$fk_port <- as.numeric(as.factor(data$fk_port))
 data_ia$fk_pied <- as.numeric(as.factor(data$fk_pied))
 data_ia$fk_situation <- as.numeric(as.factor(data$fk_situation))
 data_ia$fk_revetement <- as.numeric(as.factor(data$fk_revetement))
-data_ia$dte_plantation <- as.numeric(as.factor(data$dte_plantation))
 data_ia$fk_revetement <- as.numeric(as.factor(data$fk_revetement))
-data_ia$dte_abattage <- as.numeric(as.factor(data$dte_abattage))
 data_ia$villeca <- as.numeric(as.factor(data$villeca))
 data_ia$nomfrancais <- as.numeric(as.factor(data$nomfrancais))
-data_ia$GlobalID <- as.numeric(as.factor(data$GlobalID))
-data_ia$nomfrancais <- as.numeric(as.factor(data$nomfrancais))
-data_ia$GlobalID <- as.numeric(as.factor(data$GlobalID))
-data_ia$nomfrancais <- as.numeric(as.factor(data$nomfrancais))
-data_ia$GlobalID <- as.numeric(as.factor(data$GlobalID))
 data_ia$feuillage <- as.numeric(as.factor(data$feuillage))
 data_ia$remarquable <- as.numeric(as.factor(data$remarquable))
