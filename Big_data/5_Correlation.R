c97831276f4f3aef1e83b754fc6d3aa097ea43d0
vincent <3millestone@gmail.com>
2024-06-13 11:52:13 +0200

    sauvegarde ia en csv prediction age

--- Big_data/5_Correlation.R
+++ Big_data/5_Correlation.R
@@ -140,6 +140,32 @@ for (variable in variables) {
 }
 
 
+# | ------------------------------------------ |
+# |------| PPREDICTION PROCHAIN ARBRE |------|
+# | ------------------------------------------ |
 
 
+# | ------------------------------------------ |
+# |------| PREDICTION AGE ARBRE |------|
+# | ------------------------------------------ |
+data_ia$fk_arb_etat <- as.numeric(as.factor(data$fk_arb_etat))
+data_ia$fk_stadedev <- as.numeric(as.factor(data$fk_stadedev))
+data_ia$fk_situation <- as.numeric(as.factor(data$fk_situation))
+data_ia$remarquable <- as.numeric(as.factor(data$remarquable))
+test_data <- data
+model_age <- lm(age_estim ~ haut_tronc + tronc_diam, data = train_data)
 
+# PRERDIR sur les données de test
+test_data$age_pred <- predict(model_age, newdata = test_data)
+
+# CALCULER les écarts sur test
+test_data$age_resid <- abs(test_data$age_estim - test_data$age_pred)
+
+# Graphique des écarts positifs vs valeurs réelles avec ligne moyenne
+ggplot(test_data, aes(x = age_estim, y = age_resid)) +
+  geom_point(alpha = 0.3) +
+  geom_hline(yintercept = mean(test_data$age_resid), color = "red", linetype = "dashed") +
+  labs(title = "Écarts Absolus vs Âge (année)", x = "Âge (années)", y = "Écarts Absolus") + 
+  theme_minimal() + 
+  annotate("text", x = max(test_data$age_estim) - 20, y = mean(test_data$age_resid) + 1.5,label = paste("Moyenne des écarts =", round(mean(test_data$age_resid), 2)), color = "red")
+  
