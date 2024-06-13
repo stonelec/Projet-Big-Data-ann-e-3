c511ecc4a6e35e4b2c4aa023a8c8347316fa345f
vincent <3millestone@gmail.com>
2024-06-13 14:41:47 +0200

    correction code prediction age

--- Big_data/9_Prediction.R
+++ Big_data/9_Prediction.R
@@ -1,30 +1,19 @@
-# | ======================================== |
-# |======| PPREDICTION PROCHAIN ARBRE |======|
-# | ======================================== |
-
-
 # | =================================== |
 # |======| PPREDICTION AGE ARBRE |======|
 # | =================================== |
 
-
-data_ia$fk_arb_etat <- as.numeric(as.factor(data$fk_arb_etat)) # ça sert a rien gros fdp
-data_ia$fk_stadedev <- as.numeric(as.factor(data$fk_stadedev)) # ça sert a rien gros fdp
-data_ia$fk_situation <- as.numeric(as.factor(data$fk_situation)) # ça sert a rien gros fdp
-data_ia$remarquable <- as.numeric(as.factor(data$remarquable)) # ça sert a rien gros fdp
-test_data <- data # pourquoi pas la version full num ? gros fdp
-#model_age <- lm(age_estim ~ fk_arb_etat + haut_tronc + tronc_diam + fk_stadedev + fk_situation + remarquable, data = train_data)
-model_age <- lm(age_estim ~ haut_tronc + tronc_diam, data = train_data)
+#CREER un modèle de l'âge à partir des dimensions physique des arbres
+model_age <- lm(age_estim ~ haut_tronc + tronc_diam, data = data)
 # PRERDIR sur les données de test
-test_data$age_pred <- predict(model_age, newdata = test_data)
+data$age_pred <- predict(model_age, newdata = data)
 summary(model_age)
 # CALCULER les écarts sur test
-test_data$age_resid <- abs(test_data$age_estim - test_data$age_pred)
+data$age_resid <- abs(data$age_estim - data$age_pred)
 
 # Graphique des écarts positifs vs valeurs réelles avec ligne moyenne
-ggplot(test_data, aes(x = age_estim, y = age_resid)) +
+ggplot(data, aes(x = age_estim, y = age_resid)) +
   geom_point(alpha = 0.3) +
-  geom_hline(yintercept = mean(test_data$age_resid), color = "red", linetype = "dashed") +
+  geom_hline(yintercept = mean(data$age_resid), color = "red", linetype = "dashed") +
   labs(title = "Écarts Absolus vs Âge (année)", x = "Âge (années)", y = "Écarts Absolus") + 
   theme_minimal() + 
-  annotate("text", x = max(test_data$age_estim) - 20, y = mean(test_data$age_resid) + 1.5,label = paste("Moyenne des écarts =", round(mean(test_data$age_resid), 2)), color = "red")
+  annotate("text", x = max(data$age_estim) - 20, y = mean(data$age_resid) + 1.5,label = paste("Moyenne des écarts =", round(mean(data$age_resid), 2)), color = "red")
