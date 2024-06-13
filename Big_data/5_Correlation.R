4cfa27d88ccf604a7903541e8aced8b9d8c37a4f
mpait <mathias.paitier@isen-ouest.yncrea.fr>
2024-06-13 11:50:57 +0200

    Correlation --> mise en page Stats_descriptives --> moyennes, médianes et ecarts-types pour age,haut tronc, haut tot, diam tronc sur toutes les qualitatives

--- Big_data/5_Correlation.R
+++ Big_data/5_Correlation.R
@@ -1,6 +1,43 @@
-# | -------------------------------------- |
-# |------| Corélations avec pearson |------|
-# | -------------------------------------- | 
+# | ====================================== |
+# |======| Corélations avec pearson |======|
+# | ====================================== | 
+
+
+# | ------------------------------------ |
+# |------| Matrice de corrélation |------|
+# | ------------------------------------ |
+
+# Calculer la matrice de corrélation
+cor_matrix <- cor(data_ia, method = "pearson")
+# Afficher la matrice de corrélation
+print(cor_matrix)
+# Transformer la matrice de corrélation en un format long pour ggplot
+melted_cor_matrix <- melt(cor_matrix)
+# Créer un heatmap avec ggplot2
+ggplot(data = melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1, 1), space = "Lab", 
+                       name="Correlation") +
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1)) +
+  coord_fixed() +
+  labs(title = "Matrice de Corrélation",
+       x = "Variables",
+       y = "Variables") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.ticks = element_blank())
+
+
+# | --------------------------------------- |
+# |------| Graphiques de corrélation |------|
+# | --------------------------------------- |
+
 
 cor_age_diam <- cor(data$age_estim, data$tronc_diam, method = "pearson")
 graphe_cor_age_diam <- ggplot(data, aes(x = age_estim, y = tronc_diam)) +
@@ -52,47 +89,9 @@ graphe_cor_haut_diam
 
 
 
-
-
-
-
-# Calculer la matrice de corrélation
-cor_matrix <- cor(data_ia, method = "pearson")
-
-# Afficher la matrice de corrélation
-print(cor_matrix)
-
-# Transformer la matrice de corrélation en un format long pour ggplot
-melted_cor_matrix <- melt(cor_matrix)
-
-# Créer un heatmap avec ggplot2
-ggplot(data = melted_cor_matrix, aes(x = Var1, y = Var2, fill = value)) +
-  geom_tile() +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1, 1), space = "Lab", 
-                       name="Correlation") +
-  theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1)) +
-  coord_fixed() +
-  labs(title = "Heatmap de la Matrice de Corrélation",
-       x = "Variables",
-       y = "Variables") +
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.ticks = element_blank())
-
-
-
-
-
-
-# | ------------------------------------- |
-# |------| Tableaux de contingence |------|
-# | ------------------------------------- | 
+# | ===================================== |
+# |======| Tableaux de contingence |======|
+# | ===================================== | 
 
 # ------------- Remarquable // Stade de développement -------------
 
@@ -248,81 +247,4 @@ cont_age_dev <- table(data$age_estim, data$fk_stadedev)
 mosaicplot(cont_age_dev, main = "Graphique Mozaïque : Stade de développement vs. Age", color = TRUE, las=1)
 
 cont_age_dev
-
-
-
-# | --------------------------------------------------- |
-# |------| Ecarts de moyennes entre deux groupes |------|
-# | --------------------------------------------------- | 
-
-# Liste des variables à comparer
-variables <- c("age_estim", "haut_tot", "tronc_diam")
-
-# ------------------ Type de feuillage ------------------
-
-# Comparaison des moyennes pour chaque variable
-for (variable in variables) {
-  cat("\nComparaison des moyennes pour:", variable, "\n")
-  
-  # Extraire les données pour chaque groupe
-  conifere_values <- data_conifere[[variable]]
-  feuillu_values <- data_feuillu[[variable]]
-  
-  # Effectuer le test t de Student
-  t_test_result_feuill <- t.test(conifere_values, feuillu_values)
-  
-  # Afficher les résultats
-  cat("Moyenne (Conifère):", mean(conifere_values), "\n")
-  cat("Moyenne (Feuillu):", mean(feuillu_values), "\n")
-  cat("t-value:", t_test_result_feuill$statistic, "\n")
-  cat("p-value:", t_test_result_feuill$p.value, "\n")
-  cat("Intervalle de confiance:", t_test_result_feuill$conf.int, "\n")
-}
-
-# ------------------ Remarquable ------------------
-
-# Comparaison des moyennes pour chaque variable
-for (variable in variables) {
-  cat("\nComparaison des moyennes pour:", variable, "\n")
-  
-  # Extraire les données pour chaque groupe
-  remar_non_values <- data_remarquable_non[[variable]]
-  remar_oui_values <- data_remarquable_oui[[variable]]
-  
-  # Effectuer le test t de Student
-  t_test_result_remar <- t.test(remar_non_values, remar_oui_values)
-  
-  # Afficher les résultats
-  cat("Moyenne (Remarquable=non):", mean(remar_non_values), "\n")
-  cat("Moyenne (Remarquable=oui):", mean(remar_oui_values), "\n")
-  cat("t-value:", t_test_result_remar$statistic, "\n")
-  cat("p-value:", t_test_result_remar$p.value, "\n")
-  cat("Intervalle de confiance:", t_test_result_remar$conf.int, "\n")
-}
-
-
-# ------------------ Revêtement ------------------
-
-# Comparaison des moyennes pour chaque variable
-for (variable in variables) {
-  cat("\nComparaison des moyennes pour:", variable, "\n")
-  
-  # Extraire les données pour chaque groupe
-  revet_non_values <- data_revet_non[[variable]]
-  revet_oui_values <- data_revet_oui[[variable]]
-  
-  # Effectuer le test t de Student
-  t_test_result_revet <- t.test(revet_non_values, revet_oui_values)
-  
-  # Afficher les résultats
-  cat("Moyenne (Revetement=non):", mean(revet_non_values), "\n")
-  cat("Moyenne (Revetement=oui):", mean(revet_oui_values), "\n")
-  cat("t-value:", t_test_result_revet$statistic, "\n")
-  cat("p-value:", t_test_result_revet$p.value, "\n")
-  cat("Intervalle de confiance:", t_test_result_revet$conf.int, "\n")
-}
-
-
-
-
 
