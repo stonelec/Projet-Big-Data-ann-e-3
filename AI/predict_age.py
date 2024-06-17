dfec7c966818e0972b6a75d6515e1be53ca44ac9
mpait <mathias.paitier@isen-ouest.yncrea.fr>
2024-06-17 10:28:12 +0200

    Ajout de notre csv du Big Data et fichier fonctionnalité 2

---
+++ AI/predict_age.py
@@ -0,0 +1,16 @@
+# ===============================================
+# =========== Préparation des données ===========
+# ===============================================
+
+import pandas as pd
+
+# Chargement des données
+data = pd.read_csv('AI_Patrimoine_Arboré_(RO).csv')
+
+
+
+
+
+
+
+
