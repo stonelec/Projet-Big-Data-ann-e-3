19dfb2dd66af08fc96523f16bd468d368a0012a0
mpait <mathias.paitier@isen-ouest.yncrea.fr>
2024-06-18 14:39:12 +0200

    Modif predict_age --> ajout Random forest
    On revoit l'organisation du projet

--- AI/predict_age.py
+++ AI/predict_age.py
@@ -54,8 +54,6 @@ X_test_scaled = scaler.transform(X_test)
 # ====================================== Implémentation du modèle K-Nearest Neighbors ==================================
 # ======================================================================================================================
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report, confusion_matrix
 
 # Définir la plage de nombres de voisins à tester
 range_n_neighbors = range(2, 16)
@@ -71,6 +69,44 @@ for n_neighbors in range_n_neighbors:
     tab_knn.append(knn)
     tab_pred_knn.append(y_pred)
 
+# ======================================================================================================================
+# ====================================== Implémentation du modèle K-Nearest Neighbors ==================================
+# ======================================================================================================================
+from sklearn.neighbors import KNeighborsClassifier
+
+# Définir la plage de nombres de voisins à tester
+range_n_neighbors = range(2, 16)
+
+tab_knn = []
+tab_pred_knn = []
+
+for n_neighbors in range_n_neighbors:
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train_scaled, y_train_classes)
+    y_pred_knn = knn.predict(X_test_scaled)
+
+    tab_knn.append(knn)
+    tab_pred_knn.append(y_pred_knn)
+
+# ======================================================================================================================
+# ===================================== Implémentation du modèle Random Forest Classification ==========================
+# ======================================================================================================================
+from sklearn.ensemble import RandomForestClassifier
+
+# Définir la plage de nombres d'arbres à tester
+range_n_estimators = range(10, 110, 10)  # le n_estimators est le nombre d'arbres dans la forêt; il va de 10 à 100; On teste toutes les dizaines
+n_estimators = len(range_n_estimators)
+tab_rf = []
+tab_pred_rf = []
+
+for n_estimators in range_n_estimators:
+    rf = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train_scaled, y_train_classes)
+    y_pred_rf = rf.predict(X_test_scaled)
+
+    tab_rf.append(rf)
+    tab_pred_rf.append(y_pred_rf)
+
 
 # ======================================================================================================================
 # ============================================ Évaluation et visualisation =============================================
@@ -78,180 +114,236 @@ for n_neighbors in range_n_neighbors:
 
 # ---------------------------------------- Précision en fonction du nombre de voisins ----------------------------------
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
-# Liste pour stocker les scores de précision
-accuracy_scores = []
+# ====---------------------------------------------------------------------------====
+#  ||                             K-Nearest Neighbors                             ||
+# ====---------------------------------------------------------------------------====
+accuracy_scores_knn = []
 
 for n_neighbors in range_n_neighbors:
-    # Calculer le score de précision et l'ajouter à la liste
-    accuracy = accuracy_score(y_test_classes, tab_pred_knn[n_neighbors-2])
-    accuracy_scores.append(accuracy)
+    accuracy_knn = accuracy_score(y_test_classes, tab_pred_knn[n_neighbors - 2])      # Calculer le score de précision et l'ajouter à la liste
+    accuracy_scores_knn.append(accuracy_knn)
 
-    print(f'Nombre de voisins: {n_neighbors}, accuracy: {accuracy:.4f}')
+    print(f'KNN = Nombre de voisins: {n_neighbors}, accuracy: {accuracy_knn:.4f}')
 
-mean_accuracy_score = np.mean(accuracy_scores)
-print(f'Moyenne des scores d\'accuracy: {mean_accuracy_score:.2f}')
+mean_accuracy_score_knn = np.mean(accuracy_scores_knn)
+print(f'KNN = Moyenne des scores d\'accuracy: {mean_accuracy_score_knn:.2f}')
 
 # Tracer les scores de précision pour chaque nombre de voisins
-plt.plot(range_n_neighbors, accuracy_scores, marker='o')
-plt.axhline(y=mean_accuracy_score, color='r', linestyle='--', label=f'Moyenne = {mean_accuracy_score:.2f}')
+plt.figure(figsize=(10, 6))
+plt.plot(range_n_neighbors, accuracy_scores_knn, marker='o')
+plt.axhline(y=mean_accuracy_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_accuracy_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('Précision')
 plt.title('Accuracy en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
+plt.grid(True)
+plt.show()
+
+# ====--------------------------------------------------------------------------------====
+#  ||                             Random Forest Classifier                             ||
+# ====--------------------------------------------------------------------------------====
+accuracy_scores_rf = []
+
+# Évaluation des performances pour chaque nombre d'arbres (n_estimators)
+for idx, n_estimators in enumerate(range_n_estimators):
+    # enumerate retourne à la fois l'index et la valeur de chaque élément
+    accuracy_rf = accuracy_score(y_test_classes, tab_pred_rf[idx])  # Calculer le score de précision et l'ajouter à la liste
+    accuracy_scores_rf.append(accuracy_rf)
+
+    print(f'Random Forest = Nombre d\'arbres: {n_estimators}, accuracy: {accuracy_rf:.4f}')
+
+# Calculer la moyenne des scores de précision
+mean_accuracy_score_rf = np.mean(accuracy_scores_rf)
+print(f'Random Forest = Moyenne des scores d\'accuracy: {mean_accuracy_score_rf:.2f}')
+
+# Tracer les scores de précision pour chaque nombre d'arbres
+plt.figure(figsize=(10, 6))
+plt.plot(range_n_estimators, accuracy_scores_rf, marker='o')
+plt.axhline(y=mean_accuracy_score_rf, color='r', linestyle='--', label=f'Moyenne = {mean_accuracy_score_rf:.2f}')
+plt.xlabel('Nombre d\'arbres')
+plt.ylabel('Précision')
+plt.title('Accuracy en fonction du nombre d\'arbres pour Random Forest')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
 # ---------------------------------------- Validation croisée en fonction du nombre de voisins -------------------------
 from sklearn.model_selection import cross_val_score
 
-cross_val_scores = []
+cross_val_scores_knn = []
 
 # Calculer le score de validation croisée pour chaque nombre de voisins
 for n_neighbors in range_n_neighbors:
-    # Calculer le score de validation croisée avec 5 valeurs croisées
-    scores = cross_val_score(tab_knn[n_neighbors-2], X_train_scaled, y_train_classes, cv=5, scoring='accuracy')
-    cross_val_scores.append(np.mean(scores))
+    # ====---------------------------------------------------------------------------====
+    #  ||                             K-Nearest Neighbors                             ||
+    # ====---------------------------------------------------------------------------====
+    scores_knn = cross_val_score(tab_knn[n_neighbors - 2], X_train_scaled, y_train_classes, cv=5, scoring='accuracy')   # Calculer le score de validation croisée avec 5 valeurs croisées
+    cross_val_scores_knn.append(np.mean(scores_knn))
 
-    print(f'Nombre de voisins: {n_neighbors}, score en valeur croisée: {scores:.4f}')
+    print(f'Nombre de voisins: {n_neighbors}, scores de validation croisée: {scores_knn}')
 
 # Calculer la moyenne des scores de validation croisée
-mean_cross_val_score = np.mean(cross_val_scores)
-print(f'Moyenne des scores de validation croisée en accuracy: {mean_cross_val_score:.2f}')
+mean_cross_val_score_knn = np.mean(cross_val_scores_knn)
+print(f'Moyenne des scores de validation croisée en accuracy: {mean_cross_val_score_knn:.2f}')
 
 # Tracer les scores de validation croisée pour chaque nombre de voisins
 plt.figure(figsize=(10, 6))
-plt.plot(range_n_neighbors, cross_val_scores, marker='o', linestyle='-', label='Scores de validation croisée')
-plt.axhline(y=mean_cross_val_score, color='r', linestyle='--', label=f'Moyenne = {mean_cross_val_score:.2f}')
+plt.plot(range_n_neighbors, cross_val_scores_knn, marker='o', linestyle='-', label='Scores de validation croisée')
+plt.axhline(y=mean_cross_val_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_cross_val_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('Score de validation croisée (accuracy)')
 plt.title('Score de validation croisée en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
 # -------------------------------------------- RMSE -----------------------------------------------------------
 from sklearn.metrics import mean_squared_error
 
-rmse_scores = []
+rmse_scores_knn = []
 
 # Calculer la RMSE pour chaque modèle KNN
 for n_neighbors in range_n_neighbors:
-    knn = tab_knn[n_neighbors - 2]  # Récupérer le modèle KNN correspondant
-    y_pred = tab_pred_knn[n_neighbors - 2]  # Récupérer les prédictions correspondantes
+    # ====---------------------------------------------------------------------------====
+    #  ||                             K-Nearest Neighbors                             ||
+    # ====---------------------------------------------------------------------------====
+    rmse_knn = np.sqrt(mean_squared_error(y_test_classes, tab_pred_knn[n_neighbors - 2]))
+    rmse_scores_knn.append(rmse_knn)
 
-    # Calculer la RMSE
-    rmse = np.sqrt(mean_squared_error(y_test_classes, y_pred))
-    rmse_scores.append(rmse)
-
-    print(f'Nombre de voisins: {n_neighbors}, RMSE: {rmse:.4f}')
+    print(f'Nombre de voisins: {n_neighbors}, RMSE: {rmse_knn:.4f}')
 
 # Calculer la moyenne des scores de validation croisée
-mean_rmse_score = np.mean(rmse_scores)
-print(f'Moyenne des RMSE: {mean_rmse_score:.2f}')
+mean_rmse_score_knn = np.mean(rmse_scores_knn)
+print(f'Moyenne des RMSE: {mean_rmse_score_knn:.2f}')
 
 # Tracer les scores de RMSE pour chaque nombre de voisins
 plt.figure(figsize=(10, 6))
-plt.plot(range_n_neighbors, rmse_scores, marker='o', linestyle='-')
+plt.plot(range_n_neighbors, rmse_scores_knn, marker='o', linestyle='-')
+plt.axhline(y=mean_rmse_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_rmse_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('RMSE')
 plt.title('RMSE en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
 # ------------------------------------------------- Matrice de confusion -----------------------------------------------
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
-"""
-# Visualiser la matrice de confusion
-conf_matrix = confusion_matrix(y_test_classes, y_pred)
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
-            xticklabels=['0-10', '10-50', '50-100', '100-200'],
-            yticklabels=['0-10', '10-50', '50-100', '100-200'])
-plt.xlabel('Prédictions')
-plt.ylabel('Vérités terrain')
-plt.title(f'Matrice de confusion pour K-Nearest Neighbors avec n = {num_neighbors} - score = {accuracy:.2f}')
-plt.show()
-"""
+
+for n_neighbors in range_n_neighbors:
+    # ====---------------------------------------------------------------------------====
+    #  ||                             K-Nearest Neighbors                             ||
+    # ====---------------------------------------------------------------------------====
+    conf_matrix_knn = confusion_matrix(y_test_classes, tab_pred_knn[n_neighbors - 2])
+
+    print(f'Matrice de confusion pour K-Nearest Neighbors avec n = {n_neighbors}')
+    print(conf_matrix_knn)
+    """
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(conf_matrix_knn, annot=True, fmt='d', cmap='Blues',
+                xticklabels=['0-10', '10-50', '50-100', '100-200'],
+                yticklabels=['0-10', '10-50', '50-100', '100-200'])
+    plt.xlabel('Prédictions')
+    plt.ylabel('Vérités terrain')
+    plt.title(f'Matrice de confusion pour K-Nearest Neighbors avec n = {n_neighbors} - score = {accuracy_scores_knn[n_neighbors-2]:.2f}')
+    plt.show()
+    """
+
 
 # ------------------------------------------------- Précision et Rappel ------------------------------------------------
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-precision_scores = []
-recall_scores = []
-f1_scores = []
+precision_scores_knn = []
+recall_scores_knn = []
+f1_scores_knn = []
 
 # Évaluer la précision et le rappel pour chaque nombre de voisins
 for n_neighbors in range_n_neighbors:
-    # Calcul de la précision moyenne pondérée
-    precision = precision_score(y_test_classes, tab_pred_knn[n_neighbors-2], average='weighted', zero_division=1)
+    # ====---------------------------------------------------------------------------====
+    #  ||                             K-Nearest Neighbors                             ||
+    # ====---------------------------------------------------------------------------====
+    precision_knn = precision_score(y_test_classes, tab_pred_knn[n_neighbors - 2], average='weighted', zero_division=1)     # Calcul de la précision moyenne pondérée
 
-    # Calcul du rappel moyen pondéré
-    recall = recall_score(y_test_classes, tab_pred_knn[n_neighbors-2], average='weighted', zero_division=1)
+    recall_knn = recall_score(y_test_classes, tab_pred_knn[n_neighbors - 2], average='weighted', zero_division=1)           # Calcul du rappel moyen pondéré
 
-    # Calcul du f1_score
-    f1 = f1_score(y_test_classes, tab_pred_knn[n_neighbors-2], average='weighted', zero_division=1)
+    f1_knn = f1_score(y_test_classes, tab_pred_knn[n_neighbors-2], average='weighted', zero_division=1)                     # Calcul du f1_score
 
-    precision_scores.append(precision)
-    recall_scores.append(recall)
-    f1_scores.append(f1)
+    precision_scores_knn.append(precision_knn)
+    recall_scores_knn.append(recall_knn)
+    f1_scores_knn.append(f1_knn)
 
-    print(f'Nombre de voisins: {n_neighbors}, Précision: {precision:.4f}, Rappel: {recall:.4f}, Ratio: {f1:.4f}')
+    print(f'Nombre de voisins: {n_neighbors}, Précision: {precision_knn:.4f}, Rappel: {recall_knn:.4f}, Ratio: {f1_knn:.4f}')
 
-# Calculer la moyenne des scores de validation croisée
-mean_precision_score = np.mean(precision_scores)
-print(f'Moyenne des scores de précision: {mean_precision_score:.2f}')
+# Calculer les moyennes de la précision, du rappel et du F1-score
+    # ====---------------------------------------------------------------------------====
+    #  ||                             K-Nearest Neighbors                             ||
+    # ====---------------------------------------------------------------------------====
+mean_precision_score_knn = np.mean(precision_scores_knn)
+print(f'Moyenne des scores de précision: {mean_precision_score_knn:.2f}')
+
+mean_recall_score_knn = np.mean(recall_scores_knn)
+print(f'Moyenne des scores de rappel: {mean_recall_score_knn:.2f}')
+
+mean_f1_score_knn = np.mean(f1_scores_knn)
+print(f'Moyenne des F1-score: {mean_f1_score_knn:.2f}')
+
+# Afficher les graphiques de la précision, du rappel et du F1-score
+# ====---------------------------------------------------------------------------====
+#  ||                             K-Nearest Neighbors                             ||
+# ====---------------------------------------------------------------------------====
 
 plt.figure(figsize=(10, 6))
-plt.plot(range_n_neighbors, precision_scores, marker='o', linestyle='-')
-plt.axhline(y=mean_cross_val_score, color='r', linestyle='--', label=f'Moyenne = {mean_precision_score:.2f}')
+plt.plot(range_n_neighbors, precision_scores_knn, marker='o', linestyle='-')
+plt.axhline(y=mean_cross_val_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_precision_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('Précision')
 plt.title('Précision en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
-# Calculer la moyenne des scores de validation croisée
-mean_recall_score = np.mean(recall_scores)
-print(f'Moyenne des scores de rappel: {mean_recall_score:.2f}')
-
 plt.figure(figsize=(10, 6))
-plt.plot(range_n_neighbors, recall_scores, marker='o', linestyle='-')
-plt.axhline(y=mean_cross_val_score, color='r', linestyle='--', label=f'Moyenne = {mean_recall_score:.2f}')
+plt.plot(range_n_neighbors, recall_scores_knn, marker='o', linestyle='-')
+plt.axhline(y=mean_cross_val_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_recall_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('Rappel')
 plt.title('Rappel en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
-# Calculer la moyenne des scores de validation croisée
-mean_f1_score = np.mean(f1_scores)
-print(f'Moyenne des F1-score: {mean_f1_score:.2f}')
-
 plt.figure(figsize=(10, 6))
-plt.plot(range_n_neighbors, f1_scores, marker='o', linestyle='-')
-plt.axhline(y=mean_cross_val_score, color='r', linestyle='--', label=f'Moyenne = {mean_f1_score:.2f}')
+plt.plot(range_n_neighbors, f1_scores_knn, marker='o', linestyle='-')
+plt.axhline(y=mean_cross_val_score_knn, color='r', linestyle='--', label=f'Moyenne = {mean_f1_score_knn:.2f}')
 plt.xlabel('Nombre de voisins')
 plt.ylabel('F1-score')
 plt.title('F1-score en fonction du nombre de voisins pour K-Nearest Neighbors')
+plt.legend(loc='best')
 plt.grid(True)
 plt.show()
 
 # ----------------------------------------------------- Courbe ROC -----------------------------------------------------
-
 from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
 
-y_prob = tab_knn[5].predict_proba(X_test_scaled)
+# ====---------------------------------------------------------------------------====
+#  ||                             K-Nearest Neighbors                             ||
+# ====---------------------------------------------------------------------------====
+y_prob_knn = tab_knn[5].predict_proba(X_test_scaled)
 
 # Binariser les classes pour chaque classe pour la courbe ROC
 y_test_bin = label_binarize(y_test_classes, classes=[0, 1, 2, 3])
 n_classes = y_test_bin.shape[1]
 
 # Calculer les courbes ROC et l'AUC pour chaque classe
-plt.figure(figsize=(8, 6))
-
+plt.figure(figsize=(10, 6))
 for i in range(n_classes):
-    fpr, tpr, _ = roc_curve(y_test_bin[:, i], y_prob[:, i])
-    roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=2, label=f'Classe {i} (AUC = {roc_auc:.2f})')
+    false_positive_rate, true_positive_rate, _ = roc_curve(y_test_bin[:, i], y_prob_knn[:, i])
+    roc_auc = auc(false_positive_rate, true_positive_rate)                      # roc_auc = roc _ area under the curve
+    plt.plot(false_positive_rate, true_positive_rate, lw=2, label=f'Classe {i} (AUC = {roc_auc:.2f})')
 
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
