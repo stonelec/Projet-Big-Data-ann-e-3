b73eba4809de04213c63a82643e3228479371606
ISEN <ethan.le-pan@isen-ouest.yncrea.fr>
2024-06-19 09:31:11 +0200

    script python fini pour le moment

--- AI/fonctionnalite-1/k-means.py
+++ AI/fonctionnalite-1/k-means.py
@@ -1,7 +1,10 @@
 import sys
 import pandas as pd
 from sklearn.cluster import KMeans
+
 from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.metrics import davies_bouldin_score
 
 data_arbre = pd.read_csv("../Data_Arbre.csv", usecols=["longitude", "latitude", "haut_tronc", "age_estim", "tronc_diam", "fk_prec_estim" ])
 
@@ -9,13 +12,15 @@ print("-----------------------------------------------------")
 print("Calcul de kmeans avec comme nombre de clusters : ", 2) #int(sys.argv[1])
 print("-----------------------------------------------------")
 
+n_clusters = 2 #int(sys.argv[1])
+
 def calcul_kmeans(n):
 
     model = KMeans(n_clusters=n, random_state=42).fit(data_arbre)
-    return model.labels_
+    labels = model.predict(data_arbre)
+    return labels
 
 #Fonction calcul calcul kmeans :
-n_clusters = 2 #int(sys.argv[1])
 print(calcul_kmeans(n_clusters))
 
 print("-------------------------------------------------------")
@@ -24,39 +29,32 @@ print("-------------------------------------------------------")
 
 def score_silhouette_coefficient(n):
 
-    kmeans = KMeans(n_clusters=n, random_state=42).fit(data_arbre)
-    return silhouette_score(data_arbre, kmeans.labels_)
+    labels = calcul_kmeans(n_clusters)
+    return silhouette_score(data_arbre, labels)
 
-#Fonction calcul calcul kmeans :
-n_clusters = 2 #int(sys.argv[1])
+#Fonction calcul du score avec silhouette :
 print(score_silhouette_coefficient(n_clusters))
 
 print("--------------------------------------------------------")
 print("Méthode pour calculer le score : Calinski-Harabasz Index")
 print("--------------------------------------------------------")
 
-from sklearn.metrics import calinski_harabasz_score
-
 def score_calinski_harabasz(n):
 
-    kmeans = KMeans(n_clusters=n, random_state=42).fit(data_arbre)
-    return calinski_harabasz_score(data_arbre, kmeans.labels_)
+    labels = calcul_kmeans(n_clusters)
+    return calinski_harabasz_score(data_arbre, labels)
 
-#Fonction calcul calcul kmeans :
-n_clusters = 2 #int(sys.argv[1])
+#Fonction calcul du score avec calinski :
 print(score_calinski_harabasz(n_clusters))
 
 print("-----------------------------------------------------")
 print("Méthode pour calculer le score : Davies-Bouldin Index")
 print("-----------------------------------------------------")
 
-from sklearn.metrics import davies_bouldin_score
-
 def score_davies_bouldin(n):
 
-    kmeans = KMeans(n_clusters=n, random_state=42).fit_predict(data_arbre)
-    return davies_bouldin_score(data_arbre, kmeans)
+    labels = calcul_kmeans(n_clusters)
+    return davies_bouldin_score(data_arbre, labels)
 
-#Fonction calcul calcul kmeans :
-n_clusters = 2 #int(sys.argv[1])
+#Fonction calcul du score avec davies kmeans :
 print(score_davies_bouldin(n_clusters))
