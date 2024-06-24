9e19f966e5b161bb3e30391bd64883b81a4d69b3
ethan <ethan.lepan@gmail.com>
2024-06-24 15:26:49 +0200

    commencement de la page connexion et ajax.js

---
+++ Web/Client/JavaScript/ajax.js
@@ -0,0 +1,43 @@
+function ajaxRequest(type, url, callback, data=null){
+    // Create XML HTTP request.
+    let xhr = new XMLHttpRequest();
+    if (type === 'GET' && data != null)
+        url += '?' + data;
+    xhr.open(type, url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Add onload function.
+    xhr.onload = () => {
+        switch (xhr.status) {
+            case 200:
+            case 201:
+                console.log(xhr.responseText)
+                callback(JSON.parse(xhr.responseText));
+                break;
+            default:
+                httpErrors(xhr.status);
+        }
+    };
+
+// Send XML HTTP request.
+    xhr.send(data);
+
+}
+function httpErrors(errorCode){
+    console.log(errorCode)
+
+    let messages=
+        {
+            400:'Requête incorrecte',
+            401:'Authentifiez vous',
+            403:'Accès refusé',
+            404:'Page non trouvée',
+            500:'Erreur interne du serveur',
+            504:'Service indisponible'
+
+        };
+    if (errorCode in messages){
+        $('#errors').html('<i class="fa fa-exclamation-circle"></i> <strong>'+ messages[errorCode]+'</strong>');
+        $('#errors').show();
+    }
+}
---
+++ Web/Client/connexion.php
@@ -0,0 +1,50 @@
+<?php
+?>
+
+
+<!-- -------------------------- -->
+<!-- ---------- HTML ---------- -->
+<!-- -------------------------- -->
+
+<html lang="fr">
+
+    <head>
+
+        <meta charset="UTF-8">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="style/connexion.css">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+        <title> Page de connexion </title>
+
+    </head>
+
+    <body>
+
+        <div title="Page de Connexion">
+
+            <p>Connectez-vous à votre compte</p>
+            <i><p>Bon retour !</p></i>
+            <i><p>Merci de renseigner les informations</p></i>
+
+            <p> Email </p>
+
+            <p> Mot de passe </p>
+
+            <p> Se rappeler de moi </p>
+            <p> Mot de passe oublié </p>
+
+            <p> Se connecter </p>
+
+            <i><p> Vous n'avez pas de compte ? </p></i>
+            <p> S'enregistrer </p>
+
+        </div>
+
+    </body>
+
+</html>
