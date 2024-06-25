61e3baa99436aec0e8f624d6b53704e120f86ae0
ethan <ethan.lepan@gmail.com>
2024-06-25 14:42:29 +0200

    ajax

--- Web/Client/JavaScript/affichage.js
+++ Web/Client/JavaScript/affichage.js
@@ -33,7 +33,7 @@ $("#bouton_age").click(function (){
 
 $(document).ready(function(){
 
-    ajaxRequest('GET', 'PHP/request.php', function(response) {
+    ajaxRequest('GET', 'PHP/request.php' + $id_arbre, function(response) {
         console.log('Réponse du serveur : ', response);
     }, 'action=hauteur');
 
