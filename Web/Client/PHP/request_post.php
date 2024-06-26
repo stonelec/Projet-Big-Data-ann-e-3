7e717a15102a19dac23f7e637f97533214e38d42
ethan <ethan.lepan@gmail.com>
2024-06-26 20:45:37 +0200

    fix erreurs

--- Web/Client/PHP/request_post.php
+++ Web/Client/PHP/request_post.php
@@ -91,8 +91,8 @@ switch ($requestMethod) {
                     $val_diametre_tronc != NULL && $val_etat != NULL && $val_stade != NULL && $val_port != NULL && $val_pied != NULL &&
                     $val_espece != NULL && $val_remarquable != NULL && $val_feuillage != NULL && $val_age_estime != NULL &&
                     $val_revetement != NULL){
-                    $result = addNewArbre($id_user, $val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc, $val_etat, $val_stade,
-                                $val_port, $val_pied,$val_espece, $val_remarquable, $val_feuillage, $val_age_estime, $val_revetement);
+                    $result = Arbre::addNewArbre($id_user, $val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc, $val_etat, $val_stade,
+                                $val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime, $val_revetement);
                     echo json_encode($result);
                 }
                 break;
