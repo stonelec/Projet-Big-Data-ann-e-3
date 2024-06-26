0115f3c5e9e05fc69d8186839224debfed2c8ec9
ethan <ethan.lepan@gmail.com>
2024-06-26 13:38:57 +0200

    clean le fichier

--- Web/Client/JavaScript/ajouter_arbre.js
+++ Web/Client/JavaScript/ajouter_arbre.js
@@ -4,7 +4,11 @@
 
 $(document).ready(function (){
 
-    let options;
+    let option_etat;
+    let option_stade;
+    let option_pied;
+    let option_port;
+    let option_feuillage;
 
     // --------------------------
     // ----- Noms des etats -----
@@ -17,12 +21,12 @@ $(document).ready(function (){
 
         for(let i=0; i<response.length; i++) {
 
-            options += '<option>' + response[i].etat_arb + '</option>';
+            option_etat += '<option>' + response[i].etat_arb + '</option>';
 
         }
 
         $('#option_etat').html('<option selected disabled="disabled">Please select</option>' +
-            options
+            option_etat
         );
 
     });
@@ -38,12 +42,12 @@ $(document).ready(function (){
 
         for(let i=0; i<response.length; i++) {
 
-            options += '<option>' + response[i].stade_dev + '</option>';
+            option_stade += '<option>' + response[i].stade_dev + '</option>';
 
         }
 
         $('#option_stade').html('<option selected disabled="disabled">Please select</option>' +
-            options
+            option_stade
         );
 
     });
@@ -60,12 +64,12 @@ $(document).ready(function (){
 
         for(let i=0; i<response.length; i++) {
 
-            options += '<option>' + response[i].type_pied + '</option>';
+            option_pied += '<option>' + response[i].type_pied + '</option>';
 
         }
 
         $('#option_pied').html('<option selected disabled="disabled">Please select</option>' +
-            options
+            option_pied
         );
 
     });
@@ -81,12 +85,12 @@ $(document).ready(function (){
 
         for(let i=0; i<response.length; i++) {
 
-            options += '<option>' + response[i].type_port + '</option>';
+            option_port += '<option>' + response[i].type_port + '</option>';
 
         }
 
         $('#option_port').html('<option selected disabled="disabled">Please select</option>' +
-            options
+            option_port
         );
 
     });
@@ -102,12 +106,12 @@ $(document).ready(function (){
 
         for(let i=0; i<response.length; i++) {
 
-            options += '<option>' + response[i].feuillage + '</option>';
+            option_feuillage += '<option>' + response[i].feuillage + '</option>';
 
         }
 
         $('#option_feuillage').html('<option selected disabled="disabled">Please select</option>' +
-            options
+            option_feuillage
         );
 
     });
