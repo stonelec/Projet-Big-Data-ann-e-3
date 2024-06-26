a380e5977e3f5ee7fe14201eb8079416f6cb9f46
mpait <mathias.paitier@isen-ouest.yncrea.fr>
2024-06-26 15:06:17 +0200

    Prédiction de cluster de taille

--- Web/Client/PHP/request.php
+++ Web/Client/PHP/request.php
@@ -120,7 +120,7 @@ switch ($requestAction) {
             // $command = " python3 fonc1.py 50 10 20 kmeans "
             exec($command,$output, $result);
 
-            echo json_encode($result);
+            echo json_encode($output);
         } else {
             // Retourner une erreur si l'ID n'est pas fourni
             echo json_encode(['error' => 'ID de l\'arbre manquant']);
