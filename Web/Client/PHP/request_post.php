d1e68bf3ac53a19e5b04b7fb1a641f5e1b62f08f
ethan <ethan.lepan@gmail.com>
2024-06-27 11:19:41 +0200

    test des valeurs avec leurs id

--- Web/Client/PHP/request_post.php
+++ Web/Client/PHP/request_post.php
@@ -145,10 +145,8 @@ switch ($requestMethod) {
                 echo json_encode($tab);
 
                 */
-                if($val_latitude != NULL && $val_hauteur_totale != NULL && $val_hauteur_tronc != NULL &&
-                    $val_diametre_tronc != NULL && $val_etat != NULL && $val_stade != NULL && $val_port != NULL && $val_pied != NULL &&
-                    $val_espece != NULL && $val_remarquable != NULL && $val_feuillage != NULL && $val_age_estime != NULL &&
-                    $val_revetement != NULL){
+                if($val_latitude != NULL && $val_hauteur_totale != NULL && $val_hauteur_tronc != NULL
+                    ){
 
                     /*
                     $result = Arbre::addNewArbre($id_user, $val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc, $val_etat, $val_stade,
