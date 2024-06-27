6db760a63d18b675864d05ad2375258ef405e5e8
ethan <ethan.lepan@gmail.com>
2024-06-27 09:56:00 +0200

    une solution ?

--- Web/Client/PHP/request_post.php
+++ Web/Client/PHP/request_post.php
@@ -94,13 +94,14 @@ switch ($requestMethod) {
 
             case 'ajouter_toutes_valeur':
 
+                /*
                 $tab = [$val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc,
                     $val_etat,$val_stade,$val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime,
                     $val_revetement];
 
                 echo json_encode($tab);
+                */
 
-                /*
                 if($id_user != NULL && $val_longitude != NULL && $val_latitude != NULL && $val_hauteur_totale != NULL && $val_hauteur_tronc != NULL &&
                     $val_diametre_tronc != NULL && $val_etat != NULL && $val_stade != NULL && $val_port != NULL && $val_pied != NULL &&
                     $val_espece != NULL && $val_remarquable != NULL && $val_feuillage != NULL && $val_age_estime != NULL &&
@@ -114,7 +115,7 @@ switch ($requestMethod) {
                     $result = "Problème dans les variables";
                     echo json_encode($result);
 
-                }*/
+                }
 
                 break;
 
