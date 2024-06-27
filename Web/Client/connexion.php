22672530d319bd4e8468a8f561bb4db74e777419
ethan <ethan.lepan@gmail.com>
2024-06-27 23:56:59 +0200

    test avec .php

--- Web/Client/connexion.php
+++ Web/Client/connexion.php
@@ -31,9 +31,9 @@
 </head>
 
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once('PHP/Utilisateur.php');
 $result = Utilisateur::connexion_utilisateur();
