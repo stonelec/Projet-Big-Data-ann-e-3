2fc231305a846599ca2f816854d90edf9f147503
ethan <ethan.lepan@gmail.com>
2024-06-27 11:10:16 +0200

    test des valeurs avec leurs id

--- Web/Client/PHP/request_post.php
+++ Web/Client/PHP/request_post.php
@@ -141,27 +141,30 @@ switch ($requestMethod) {
                         $val_etat,$val_stade,$val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime,
                         $val_revetement];
 
+                echo json_encode($tab);
+
+                /*
                 if($id_user != NULL && $val_longitude != NULL && $val_latitude != NULL && $val_hauteur_totale != NULL && $val_hauteur_tronc != NULL &&
                     $val_diametre_tronc != NULL && $val_etat != NULL && $val_stade != NULL && $val_port != NULL && $val_pied != NULL &&
                     $val_espece != NULL && $val_remarquable != NULL && $val_feuillage != NULL && $val_age_estime != NULL &&
                     $val_revetement != NULL){
 
-                    /*
+
                     $result = Arbre::addNewArbre($id_user, $val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc, $val_etat, $val_stade,
                         $val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime, $val_revetement);
                     echo json_encode($result);
-                    */
 
-                    echo json_encode("toutes les valeurs sont bonnes : ", $tab);
+
+                    echo json_encode("toutes les valeurs sont bonnes : ");
 
                 }
 
                 else{
 
-                    echo json_encode("Problèmes dans les variables : ", $tab);
+                    echo json_encode("Problèmes dans les variables : ");
 
                 }
-
+                */
                 break;
 
             default:
