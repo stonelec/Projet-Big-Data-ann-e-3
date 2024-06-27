2fc961ae9f6c9c1c46586fefcfbbea21c62c85ec
ethan <ethan.lepan@gmail.com>
2024-06-27 16:49:15 +0200

    connexion test

--- Web/Client/JavaScript/connexion.js
+++ Web/Client/JavaScript/connexion.js
@@ -22,25 +22,14 @@ $(document).ready(function (){
 
             let n = data_mail.length;
 
-
-            for(let i=0; i<n+1; i++){
-
-                data_mail[i] = response_mail[i];
-
-            }
-
             ajaxRequest('GET', 'PHP/request.php?action=recuperermdp',function(response_mdp)  {
 
                 console.log(response_mdp);
 
-                for(let i=0; i<response_mdp.length; i++){
+                for(let i=0; i<n; i++){
 
                     data_mot_de_passe[i] = response_mdp[i];
-
-                }
-
-
-                for(let i=0; i<n+1 ;i++){
+                    data_mail[i] = response_mail[i];
 
                     console.log(data_mail[i].email_user);
                     console.log(data_mot_de_passe[i].password_user);
