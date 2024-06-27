3890b79bf6c7e4fed885d04d198d4da0cfe976db
ethan <ethan.lepan@gmail.com>
2024-06-27 11:06:08 +0200

    test des valeurs avec leurs id

--- Web/Client/PHP/request_post.php
+++ Web/Client/PHP/request_post.php
@@ -131,19 +131,11 @@ switch ($requestMethod) {
         }
         break;
 
-
     case 'POST':
 
         switch ($requestAction_post){
 
             case 'ajouter_toutes_valeur':
-
-                /*
-                $tab = [$val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc,
-                    $val_etat,$val_stade,$val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime,
-                    $val_revetement];
-
-                echo json_encode($tab);*/
 
 
                 if($id_user != NULL && $val_longitude != NULL && $val_latitude != NULL && $val_hauteur_totale != NULL && $val_hauteur_tronc != NULL &&
@@ -151,16 +143,25 @@ switch ($requestMethod) {
                     $val_espece != NULL && $val_remarquable != NULL && $val_feuillage != NULL && $val_age_estime != NULL &&
                     $val_revetement != NULL){
 
+                    /*
                     $result = Arbre::addNewArbre($id_user, $val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc, $val_etat, $val_stade,
                         $val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime, $val_revetement);
                     echo json_encode($result);
+                    */
+
+                    $tab = [$val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc,
+                        $val_etat,$val_stade,$val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime,
+                        $val_revetement];
+                    echo json_encode("toutes les valeurs sont bonnes : ",$tab);
 
                 }
 
                 else{
 
-                    $result = "Problème dans les variables";
-                    echo json_encode($result);
+                    $tab = [$val_longitude, $val_latitude, $val_hauteur_totale, $val_hauteur_tronc, $val_diametre_tronc,
+                        $val_etat,$val_stade,$val_port, $val_pied, $val_espece, $val_remarquable, $val_feuillage, $val_age_estime,
+                        $val_revetement];
+                    echo json_encode("Problèmes dans les variables : ", $tab);
 
                 }
 
