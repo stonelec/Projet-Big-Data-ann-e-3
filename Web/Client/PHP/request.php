3f7f0c13134a851b0cdd70e2a660a773e08dc20b
ethan <ethan.lepan@gmail.com>
2024-06-28 08:45:00 +0200

    test pour recup id

--- Web/Client/PHP/request.php
+++ Web/Client/PHP/request.php
@@ -146,8 +146,8 @@ switch ($requestAction) {
         break;
 
     case 'id_du_user' :
-        $result = User::getId($mail,$password);
-        echo json_encode("proute_de_pipi");
+        //$result = User::getId($mail,$password);
+        echo json_encode($mail);
         break;
 
 # ----------------------- Prédire les infos -----------------------
