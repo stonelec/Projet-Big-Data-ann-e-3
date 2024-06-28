5250f22e7b60e77106185a894a56541790e8fc57
ethan <ethan.lepan@gmail.com>
2024-06-28 09:03:13 +0200

    test pour recup id

--- Web/Client/JavaScript/connexion.js
+++ Web/Client/JavaScript/connexion.js
@@ -15,8 +15,9 @@ $(document).ready(function (){
 
         console.log(val_email);
         console.log(mot_de_passe);
+
         let data = []
-        data = ['action=id_du_user&mot_de_passe=' + mot_de_passe + '&email=' + val_email]
+        data = ['action=id_du_user ' + '&mot_de_passe=' + mot_de_passe + '&email=' + val_email]
 
         ajaxRequest('GET', 'PHP/request.php', function (response){
 
